91bc0a12a16a30fdd5f014dec2a5617ef6a4185b
Nabille Lopez <nabillelopez@iMac-de-Nabille.local>
2024-07-17 19:14:45 -0600

    first commit

---
+++ sesion1/ejercicio1.kt
@@ -0,0 +1,11 @@
+fun main() { 
+val nombreProducto: String = "Laptop Gaming"
+  val precio: Double = 1299.99
+  val disponible: Boolean = true
+  val codigoProducto: String = "DELL-001"
+
+  println("Producto: $nombreProducto")
+  println("Precio: $precio")
+  println("Disponible: $disponible")
+  println("Código: $codigoProducto")
+}
